68c463e7e037856079ca11d8c3ae1c46c0b2d23c
tcultivator <whypanahon06@gmail.com>
2025-07-21 01:51:59 +0800

    Add files via upload

---
+++ changePassword.js
@@ -0,0 +1,34 @@
+import { apiReq } from './fetchReq.js';
+
+const getUrl = new URLSearchParams(window.location.search)
+const actualUrl = getUrl.get('email')
+console.log('eto ung email mo', actualUrl)
+
+const newPass = document.getElementById('newPass')
+const errorNotif = document.getElementById('errorNotif')
+const errormessage = document.getElementById('errormessage')
+const successNotif = document.getElementById('successNotif')
+const successmessage = document.getElementById('successmessage')
+
+
+document.getElementById('subBtn').addEventListener('click', async () => {
+    const changePassword = await apiReq('/changePassword', {
+        email: actualUrl,
+        newPass: newPass.value
+    })
+
+    if (changePassword.ok) {
+        successNotif.style.display = 'flex'
+        successmessage.textContent = changePassword.data.message
+        setTimeout(() => {
+            successNotif.style.display = 'none'
+            window.location.replace('index.html')
+        }, 1500);
+    } else {
+        errorNotif.style.display = 'flex'
+        errormessage.textContent = changePassword.data.message
+        timeoutInterval = setTimeout(() => {
+            errorNotif.style.display = 'none'
+        }, 1500);
+    }
+})
