97187a3d7d2d2b025ae1cade9d6050aa8cd00b61
tcultivator <whypanahon06@gmail.com>
2025-07-25 09:33:09 +0800

    Add files via upload

---
+++ helper/displayNewMessageAtHistory.js
@@ -0,0 +1,13 @@
+import { apiReq } from '../utils/fetchReq.js';
+
+export async function displayNewMessageAtHistory(senderId, recieverId) {
+    const getNewMessageToDisplayAtHistory = await apiReq('/getNewMessageToDisplayAtHistory', {
+        senderId: senderId,
+        recieverId: recieverId
+    })
+    if (getNewMessageToDisplayAtHistory.ok) {
+        return getNewMessageToDisplayAtHistory.data
+    } else {
+        return null
+    }
+}
