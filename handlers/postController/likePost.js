cb92c1cdd9999a0755652264b499058e57e8885c
tcultivator <whypanahon06@gmail.com>
2025-08-01 11:49:06 +0800

    Update likePost.js

--- handlers/postController/likePost.js
+++ handlers/postController/likePost.js
@@ -1,5 +1,6 @@
 import { apiReq } from "../../utils/fetchReq.js";
-export default async function likeThisPost(targetElement, loginUserId, socket) {
+import { sendNotif } from "../notificationController/sendNotifcationHandler.js";
+export default async function likeThisPost(targetElement, loginUserId, socket, loginUsername, loginProfileimage) {
     const likeIcon = targetElement.closest('#postcontent').querySelector('#likethisPost')
     if (likeIcon.style.color == 'red') {
         console.log('na like mo nato')
@@ -12,6 +13,7 @@ export default async function likeThisPost(targetElement, loginUserId, socket) {
         likeIcon.style = 'color:rgb(36, 36, 36)'
         likeIcon.classList = 'fa-regular fa-heart'
         const postId = targetElement.dataset.id
+
         console.log(loginUserId)
         console.log(postId)
         const unlikeThisPost = await apiReq('/unlikeThisPost', { postId: postId, userId: loginUserId })
@@ -35,15 +37,16 @@ export default async function likeThisPost(targetElement, loginUserId, socket) {
         likeIcon.style = 'color:red'
         likeIcon.classList = 'fa-solid fa-heart'
         const postId = targetElement.dataset.id
-        console.log(loginUserId)
-        console.log(postId)
+        const userPostId = document.querySelector(`#postcontent[data-id="${postId}"]`).querySelector('#userThatPost')
+        console.log('eto ung useId ng nag post ', userPostId.dataset.userid)
         const likeThisPost = await apiReq('/likeThisPost', { postId: postId, userId: loginUserId })
         if (likeThisPost.ok) {
             console.log('you like this post that id is ', postId)
             console.log(likeIcon.style.color)
             console.log(likeCountLabel)
+            console.log(likeThisPost)
             socket.emit('userLikeThisPost', { postId, likeCount })
-
+            sendNotif(userPostId.dataset.userid, loginUserId, loginUsername, loginProfileimage, likeThisPost.data.message)
         } else {
 
             console.log('error sa like')
