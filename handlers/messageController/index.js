ee99fef84d36ef0c916c02dde6cfa9313606a78d
tcultivator <whypanahon06@gmail.com>
2025-08-02 10:53:19 +0800

    Update index.js

--- handlers/messageController/index.js
+++ handlers/messageController/index.js
@@ -2,3 +2,5 @@ export { default as sendFileAsMessage } from './sendFileMessageHandler.js'
 export { default as openConvoWindow } from './openConvoWindowHandler.js'
 export { default as openMessageWindow } from './openMessageWindowHandler.js'
 export { default as sendThisMessage } from './sendMessageHandler.js'
+export { default as checkIfHasNewMessage } from './checkIfhasNewMessage.js'
+
