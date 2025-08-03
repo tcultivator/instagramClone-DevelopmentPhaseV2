010d98493c3e144c45bf05828633085dd1950899
tcultivator <whypanahon06@gmail.com>
2025-08-03 19:56:02 +0800

    Update markReadNotificationHandler.js

--- handlers/notificationController/markReadNotificationHandler.js
+++ handlers/notificationController/markReadNotificationHandler.js
@@ -1,12 +1,18 @@
 import { apiReq } from "../../utils/fetchReq.js";
-
+import { getUnreadNotif } from './getUnreadNotifCount.js'
 export async function markReadThisNotif(notifId) {
     try {
+        const elementThatMarkRead = document.querySelector(`#notifContentDetails[data-id="${notifId}"]`)
+        const buttonMarkRead = document.querySelector(`#markReadThisNotif[data-id="${notifId}"]`)
+        console.log('eto ung element na mark read, ', elementThatMarkRead)
+        elementThatMarkRead.style = 'background-color: #eaeaea;'
+        buttonMarkRead.disabled = true
         const markRead = await apiReq('/markReadThisNotif', {
             notifId: notifId
         })
         if (markRead.ok) {
             console.log('success markread')
+            getUnreadNotif();
         } else {
             console.log('error mark read')
         }
