009ecea8770745bd989923b94233da00f0c1652b
tcultivator <whypanahon06@gmail.com>
2025-08-06 04:10:59 +0800

    Delete messageController directory

--- messageController/index.js
+++
@@ -1,8 +0,0 @@
-export { default as sendFileAsMessage } from './sendFileMessageHandler.js'
-export { default as openConvoWindow } from './openConvoWindowHandler.js'
-export { default as openMessageWindow } from './openMessageWindowHandler.js'
-export { default as sendThisMessage } from './sendMessageHandler.js'
-export { default as checkIfHasNewMessage } from './checkIfhasNewMessage.js'
-export { default as displayMessageRequest } from './displayAllMessageRequestHandler.js'
-export { default as acceptMessageRequest } from './acceptMessageReqHandler.js'
-export { default as getMessageRequestCounts } from './messageRequestCountHandler.js'
